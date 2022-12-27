153327113b8c6480865bf2845387049950122ff5
Yogesh Patel <yogeshp614@gmail.com>
2022-12-26 23:30:47 -0500

    Initial Commit

---
+++ Lottie CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottie CollectionView
+//
+//  Created by Yogesh Patel on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
